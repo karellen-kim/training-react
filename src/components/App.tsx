6d2ffc81a64c308d47e330a9aaad9bcf9e5076e6
karellen-kim <vies00@hanmail.net>
2018-12-11 01:31:50 +0900

    add react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Hello from "./Hello";
 
-export default class App extends React.Component {
-  render() {
-    return <div>Test</div>
-  }
-}
+export const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Hello} />
+    </div>
+  </Router>
+);
